a2bfd198fa8471e4e78c78a3fdb19a2f4cd87768
Violin <violinwang@mail.ustc.edu.cn>
2022-05-09 23:02:43 +0800

    add road_sensor more information

--- src/sensor/road_sensor.cpp
+++ src/sensor/road_sensor.cpp
@@ -23,6 +23,21 @@ namespace Sensor {
        RM_SetWorldPosition(posHandle, s.x, s.y, s.z, s.h, s.p, s.r);
        boost::python::list roads;
        int roadNum = RM_GetNumberOfRoadsOverlapping(posHandle);
-       return boost::python::make_tuple(sensorType, roadNum); // TODO: add more road info
+       for(int i = 0; i < roadNum; i++) {
+           boost::python::list lanes;
+           int roadId = RM_GetOverlappingRoadId(posHandle, i);
+           int laneNum = RM_GetRoadNumberOfLanes(roadId, s.s);
+           for(int j = 0; j < laneNum; j++) {
+               boost::python::dict lane;
+               int laneId = RM_GetLaneIdByIndex(roadId, j, s.s);
+               float width = RM_GetLaneWidthByRoadId(roadId, laneId, s.s);
+               lane["id"] = laneId;
+               lane["width"] = width;
+               lanes.append(lane);
+           }
+           roads.append(lanes);
+       }
+
+       return boost::python::make_tuple(sensorType, roadNum, roads); // TODO: add more road info
     }
 }
